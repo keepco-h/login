43c289df65aa4e230f71d39316653fb0596c6393
keepco-h <139107496+keepco-h@users.noreply.github.com>
2025-04-04 11:44:08 +0300

    Update login.js

--- login.js
+++ login.js
@@ -8,6 +8,6 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     document.getElementById('loginMessage').textContent = "Login successful!";
   } else {
     document.getElementById('loginMessage').textContent = "Incorrect username or password.";
-    document.getElementById('loginMessage').style.color = "red";
+    document.getElementById('loginMessage').style.color = "green";
   }
 });
